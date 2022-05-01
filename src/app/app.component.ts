d051e837116e5163ed9434df63dbeddf4cd24cdc
chelmi00 <migfervil@hotmail.es>
2022-05-02 00:53:18 +0200

    Vídeo 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angular';
+export class AppComponent implements OnInit, OnDestroy {
+  intervalSub: any;
+    
+  ngOnInit() {
+    this.intervalSub = setInterval(() => {
+      console.log('Hello from ngOnInit');
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    if (this.intervalSub) {
+      clearInterval(this.intervalSub);
+    }
+  }
 }
