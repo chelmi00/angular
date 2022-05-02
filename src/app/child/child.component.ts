75192a4e42537c8d35a2860f953b8d4afe5e0796
chelmi00 <migfervil@hotmail.es>
2022-05-02 23:42:40 +0200

    Vídeo 6

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,11 +1,9 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
-  styles: ['h2 { color: pink }'],
   styleUrls: ['./child.component.css'],
-  encapsulation: ViewEncapsulation.None
 })
 export class ChildComponent implements OnInit {
   
