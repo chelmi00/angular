0c71c68f95e6bab3c2cb1cf4974a9a5bfa76c6cd
chelmi00 <migfervil@hotmail.es>
2022-05-03 22:39:10 +0200

    Vídeo 10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.None
+  template: `
+  <tr><td [attr.colspan]="1+1">one-two</td></tr>
+  <div [class.sale]="onSale">Div1</div>
+  <div [class]="'classOne classTwo'">Div2</div>
+  <div [class]="{
+    classOne: true,
+    classTwo: false
+  }">Div3</div>
+  <div [class]="['classOne', 'classTwo']">Div4</div>
+  <div [style]="'width:200px; height:40px; background:green'"></div> 
+  <div [style]="{
+    width: '50px',
+    height: '50px',
+    background: 'blue'
+  }"></div>
+  `,
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  itemImageUrl='../assets/octocat.png';
-  isUnchanged=true;
+  onSale = true;
 }
