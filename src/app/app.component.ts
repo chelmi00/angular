627f74fb3e1bdc02c2b0cd00314a55a3b9ec0842
chelmi00 <migfervil@hotmail.es>
2022-05-03 23:20:31 +0200

    Vídeo 11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,23 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <tr><td [attr.colspan]="1+1">one-two</td></tr>
-  <div [class.sale]="onSale">Div1</div>
-  <div [class]="'classOne classTwo'">Div2</div>
-  <div [class]="{
-    classOne: true,
-    classTwo: false
-  }">Div3</div>
-  <div [class]="['classOne', 'classTwo']">Div4</div>
-  <div [style]="'width:200px; height:40px; background:green'"></div> 
-  <div [style]="{
-    width: '50px',
-    height: '50px',
-    background: 'blue'
-  }"></div>
+  <button (click)="onSave()">Save</button>
+  <app-item-details (deleteRequest)="deleteItem($event)"></app-item-details>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  onSale = true;
+  onSave() {
+    console.log('click on save');
+  }
+  deleteItem(item: any) {
+    console.log(item);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HelloWorldComponent } from './hello-world.component';
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
 import { GreetingsPipePipe } from './greetings-pipe.pipe';
+import { ItemDetailsComponent } from './item-details/item-details.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { GreetingsPipePipe } from './greetings-pipe.pipe';
     HelloWorldComponent,
     ParentComponent,
     ChildComponent,
-    GreetingsPipePipe
+    GreetingsPipePipe,
+    ItemDetailsComponent
   ],
   imports: [
     BrowserModule,
